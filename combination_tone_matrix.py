67f09556facf7352de317ef21895565bedd02a2a
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-07-25 22:46:58 -0400

    add combination tone matrix for sclang

--- combination_tone_matrix.py
+++ combination_tone_matrix.py
@@ -1,16 +1,16 @@
-def get_frequency(multiplier, bass_multiple, melody):
+def get_pitch(multiplier, bass_multiple, melody):
     melody_multiple = melody * multiplier
     return bass_multiple + melody_multiple
 
 
-def get_melody_columns(multiplier, columns, bass, melody):
+def get_melody_column(multiplier, columns, bass, melody):
     bass_multiple = bass * multiplier
-    return [get_frequency(column, bass_multiple, melody) for column in columns]
+    return [get_pitch(column, bass_multiple, melody) for column in columns]
 
 
 def get_matrix(bass, melody, count=5):
     rows = range(count)
-    return [get_melody_columns(row, rows, bass, melody) for row in rows]
+    return [get_melody_column(row, rows, bass, melody) for row in rows]
 
 
 def display_matrix(matrix):
@@ -19,8 +19,9 @@ def display_matrix(matrix):
 
 
 def get_sorted_pitches(matrix, limit=None):
-    pitches = [frequency for row in matrix for frequency in row]
+    pitches = [pitch for row in matrix for pitch in row]
     pitches.sort()
+    pitches = pitches[1:]
     if not limit:
         return pitches
     return pitches[:limit]
