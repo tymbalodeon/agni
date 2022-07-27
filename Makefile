8ef94b19b89262631423e843344576d904412499
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-07-26 21:00:01 -0400

    add typehints, Makefile, and abjad.show matrix

---
+++ Makefile
@@ -0,0 +1,20 @@
+REQUIREMENTS = requirements.txt
+
+all: help
+
+add: package freeze ## pip install package and freeze requirements [arg: "package"]
+
+freeze: ## Freeze the dependencies to the requirements.txt file
+	pip freeze > $(REQUIREMENTS)
+
+help: ## Display the help menu
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | \
+	sort | \
+	awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+install: ## Install the dependencies from the requirements.txt file
+	pip install -U pip && pip install -r $(REQUIREMENTS)
+
+package:
+	pip install $(package)
+
--- combination_tone_matrix.py
+++ combination_tone_matrix.py
@@ -1,27 +1,51 @@
-def get_pitch(multiplier, bass_multiple, melody):
+from typing import Optional
+
+from abjad import Container, Duration, NamedPitch, Note, show
+
+
+def get_pitch(multiplier: int, bass_multiple: float, melody: float) -> float:
     melody_multiple = melody * multiplier
     return bass_multiple + melody_multiple
 
 
-def get_melody_column(multiplier, columns, bass, melody):
+def get_melody_column(
+    multiplier: int, columns: range, bass: float, melody: float
+) -> list[float]:
     bass_multiple = bass * multiplier
     return [get_pitch(column, bass_multiple, melody) for column in columns]
 
 
-def get_matrix(bass, melody, count=5):
+def get_matrix(bass: str, melody: str, count=5) -> list[list[float]]:
+    bass_pitch = NamedPitch(bass).hertz
+    melody_pitch = NamedPitch(melody).hertz
     rows = range(count)
-    return [get_melody_column(row, rows, bass, melody) for row in rows]
+    return [get_melody_column(row, rows, bass_pitch, melody_pitch) for row in rows]
 
 
-def display_matrix(matrix):
+def display_matrix(matrix: list[list[float]]) -> None:
     for row in matrix:
         print(row)
 
 
-def get_sorted_pitches(matrix, limit=None):
-    pitches = [pitch for row in matrix for pitch in row]
-    pitches.sort()
-    pitches = pitches[1:]
+def get_sorted_frequencies(
+    matrix: list[list[float]], limit: Optional[int] = None
+) -> list[float]:
+    frequencies = [frequency for row in matrix for frequency in row]
+    frequencies.sort()
+    frequencies = frequencies[1:]
     if not limit:
-        return pitches
-    return pitches[:limit]
+        return frequencies
+    return frequencies[:limit]
+
+
+def get_note(frequency: float) -> Note:
+    duration = Duration(1, 4)
+    pitch = NamedPitch.from_hertz(frequency)
+    return Note(pitch, duration)
+
+
+def notate_sorted_matrix(matrix: list[list[float]]) -> None:
+    sorted_frequencies = get_sorted_frequencies(matrix)
+    notes = [get_note(frequency) for frequency in sorted_frequencies]
+    notes = Container(notes)
+    show(notes)
