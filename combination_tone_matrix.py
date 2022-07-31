dd0382d8357ba7a72ac5dd349aab23576d326887
Ben Rosen <benjamin.j.rosen@gmail.com>
2022-07-31 14:32:47 -0400

    add generators

--- combination_tone_matrix.py
+++ combination_tone_matrix.py
@@ -124,31 +124,81 @@ def get_pitch_and_duration(staff):
     return [(get_pitch(note), note.written_duration) for note in notes]
 
 
-def get_pitches(current):
-    return [note.written_pitch for note in current.values()]
+def get_pitches(simultaneity):
+    return [note["current"].written_pitch for note in simultaneity.values()]
 
 
-def get_simultaneous_pitches():
-    staff_one = Staff("c,1 d", name="bass")
-    staff_two = Staff("e'2 ef g a", name="melody")
-    staff_group = StaffGroup([staff_one, staff_two])
+def get_simultaneity(staves, index):
+    return {key: value[index] for key, value in staves.items()}
+
+
+def is_end_of_passage(staff, index):
+    print(staff)
+    try:
+        if not staff:
+            return False
+        staff[index + 1]
+        return True
+    except Exception:
+        return False
+
+
+def get_shortest_note(simultaneity):
+    durations = [
+        note["current"].written_duration for note in simultaneity.values()
+    ]
+    shortest = min(durations)
+    small = [
+        simultaneity[key]
+        for key, note in simultaneity.items()
+        if note["current"].written_duration == shortest
+    ]
+    other = [
+        simultaneity[key]
+        for key, note in simultaneity.items()
+        if note["current"].written_duration != shortest
+    ]
+    return small, other
+
+
+def get_simultaneous_pitches(staff_group: StaffGroup):
     staves = {staff.name: staff for staff in staff_group.components}
-    index = 0
-    pitches = []
-    simultaneity: dict[str, Note] = {
-        key: value[index] for key, value in staves.items()
+    simultaneity: dict = {
+        key: {"current": None, "generator": iter(value)}
+        for key, value in staves.items()
     }
+    simultaneity = {
+        key: {
+            "current": next(simultaneity[key]["generator"], None),
+            "generator": simultaneity[key]["generator"],
+        }
+        for key in simultaneity.keys()
+    }
+    pitches = []
     pitches.append(get_pitches(simultaneity))
+    small, other = get_shortest_note(simultaneity)
+    print(small, other)
+    return
+    index = 0
     lowest = None
     staff_name = ""
-    for staff, note in simultaneity.items():
-        duration = note.written_duration
-        if not lowest or duration < lowest:
-            lowest = duration
-            staff_name = staff
-    simultaneity[staff_name] = staves[staff_name][index + 1]
-    pitches.append(get_pitches(simultaneity))
+    end_of_passage = is_end_of_passage(staff_name, index)
+    while not end_of_passage:
+        for staff, note in simultaneity.items():
+            duration = note.written_duration
+            if not lowest or duration < lowest:
+                lowest = duration
+                staff_name = staff
+        simultaneity[staff_name] = staves[staff_name][index + 1]
+        pitches.append(get_pitches(simultaneity))
+        end_of_passage = is_end_of_passage(
+            get_simultaneity(staves, index), index
+        )
+        index += 1
     print(pitches)
 
 
-get_simultaneous_pitches()
+staff_one = Staff("c,2 r4", name="bass")
+staff_two = Staff("e'4 ef2", name="melody")
+staff_group = StaffGroup([staff_one, staff_two])
+get_simultaneous_pitches(staff_group)
