af0d4d2cbe7a0ad6c8c65c97a1b7b468fa0a64e4
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-03-01 14:01:08 -0500

    clean up notation

--- agni/notation.py
+++ agni/notation.py
@@ -1,4 +1,3 @@
-from functools import cached_property
 from pathlib import Path
 from statistics import mode
 from typing import cast
@@ -38,6 +37,8 @@ from abjad.select import tuplets as get_tuplets
 from abjadext.rmakers import multiplied_duration
 from rich.progress import track
 
+from agni.matrix_leaf import MatrixLeaf
+
 from .helpers import get_staff_by_name, remove_none_values
 from .matrix import Matrix
 from .matrix_pitch import MatrixPitch, Tuning
@@ -52,65 +53,115 @@ class Notation:
         self._matrices = matrices
         self._passage = passage
 
-    @cached_property
+    @property
     def _number_of_matrices(self) -> int:
         return len(self._matrices)
 
-    def _get_lilypond_preamble(self, full_score=False) -> str:
+    @staticmethod
+    def _get_first_staff_leaf(staff: Staff) -> Leaf | None:
+        leaves = get_leaves(staff)
+        if not leaves:
+            return None
+        return leaves[0]
+
+    @classmethod
+    def _set_staff_instrument_name(cls, staff: Staff):
+        instrument_name = staff.name or ""
+        instrument_name = instrument_name.title()
+        first_leaf = cls._get_first_staff_leaf(staff)
+        attach(InstrumentName(instrument_name), first_leaf)
+        attach(ShortInstrumentName(instrument_name), first_leaf)
+
+    @property
+    def _input_staves(self) -> list[Staff]:
         passage = self._passage
-        if passage:
-            title = passage.title
-            composer = passage.composer
-        else:
-            if self._number_of_matrices > 1:
-                matrix_display = "Matrices"
-            else:
-                matrix_display = "Matrix"
-            title = f"Combination-Tone {matrix_display}"
-            composer = ""
-        if full_score:
-            stencils = ""
-        else:
-            stencils = """
-                \\override TimeSignature.stencil = ##f
-                \\override BarLine.stencil = ##f
-                \\override Stem.stencil = ##f
-            """
-        return f"""
-                    \\header {{
-                        tagline = ##f
-                        title = "{title}"
-                        composer = "{composer}"
-                    }}
+        if not passage:
+            return []
+        bass = passage.bass_staff
+        melody = passage.melody_staff
+        for staff in bass, melody:
+            self._set_staff_instrument_name(staff)
+        return [bass, melody]
 
-                    \\paper {{
-                        #(set-paper-size "letter")
-                        left-margin = 1.25\\in
-                        right-margin = 0.75\\in
-                        top-margin = 0.5\\in
-                        bottom-margin = 0.5\\in
-                    }}
-
-                    \\layout {{
-                        \\context {{
-                            \\Score
-                            \\numericTimeSignature
-                            {stencils}
-                        }}
-                    }}
-                """
-
-    def _show_with_preamble(
-        self, container: Component, save: bool, full_score: bool
+    @staticmethod
+    def _add_leaf_to_staff(
+        staff_group: StaffGroup,
+        instrument_name: str,
+        leaf: Leaf,
+        tuplet: Tuplet | None,
+        is_start_of_tuplet: bool,
     ):
-        lilypond_file = LilyPondFile(
-            [self._get_lilypond_preamble(full_score), container]
-        )
-        if save:
-            pdf_file_path = Path("examples") / "matrix.pdf"
-            as_pdf(lilypond_file, pdf_file_path=pdf_file_path, remove_ly=True)
+        if tuplet and is_start_of_tuplet:
+            multiplier = tuplet.multiplier
+            component: Leaf | Tuplet = Tuplet(multiplier, components=[leaf])
         else:
-            show(lilypond_file)
+            component = leaf
+        staff = next(
+            (staff for staff in staff_group if staff.name == instrument_name),
+            None,
+        )
+        if staff:
+            if tuplet and not is_start_of_tuplet:
+                parent = get_tuplets(staff)[-1]
+            else:
+                parent = staff
+            parent.append(component)
+        else:
+            staff = Staff([component], name=instrument_name)
+            instrument_name_markup = f"\\markup { {instrument_name} }"
+            instrument_name_markup = instrument_name_markup.replace("'", "")
+            first_leaf = staff[0]
+            attach(InstrumentName(instrument_name_markup), first_leaf)
+            attach(ShortInstrumentName(instrument_name_markup), first_leaf)
+            staff_group.insert(0, staff)
+
+    @classmethod
+    def _add_notes_to_staff(
+        cls, matrix_leaf: MatrixLeaf, staff_group: StaffGroup
+    ):
+        duration = matrix_leaf.duration
+        if not duration:
+            return
+        for matrix_pitch in matrix_leaf.generated_pitches:
+            note = matrix_pitch.get_note(duration, matrix_leaf.tie)
+            if not note:
+                continue
+            instrument_names = matrix_pitch.get_instrument_name()
+            cls._add_leaf_to_staff(
+                staff_group,
+                instrument_names,
+                note,
+                matrix_leaf.tuplet,
+                matrix_leaf.is_start_of_tuplet,
+            )
+
+    @classmethod
+    def _get_rest(
+        cls, duration: Duration, is_multi_measure_rest: bool
+    ) -> Rest | MultimeasureRest:
+        if is_multi_measure_rest:
+            return cast(
+                MultimeasureRest,
+                multiplied_duration([duration], MultimeasureRest)[0],
+            )
+        return Rest(duration)
+
+    @classmethod
+    def _add_rests_to_staff(
+        cls, matrix_leaf: MatrixLeaf, staff_group: StaffGroup
+    ):
+        duration = matrix_leaf.duration
+        if not duration:
+            return
+        for instrument_names in matrix_leaf.instrument_names:
+            rest = cls._get_rest(duration, matrix_leaf.is_multi_measure_rest)
+            cls._add_leaf_to_staff(
+                staff_group,
+                instrument_names,
+                rest,
+                matrix_leaf.tuplet,
+                matrix_leaf.is_start_of_tuplet,
+            )
 
     @staticmethod
     def _get_melody_note_duration(
@@ -207,25 +258,6 @@ class Notation:
             new_clef = cls._get_clef_by_octave(octave)
             if new_clef != current_clef:
                 cls._set_clef(note)
-
-    @classmethod
-    def _set_staff_group_clefs(cls, staff_group: StaffGroup):
-        staves = [
-            staff
-            for staff in staff_group
-            if staff.name not in ["bass", "melody"]
-        ]
-        for staff in staves:
-            written_pitches = [note.written_pitch for note in get_notes(staff)]
-            pitches = [note for note in written_pitches if note]
-            octaves = [pitch.octave.number for pitch in pitches]
-            octave = mode(octaves)
-            clef = cls._get_clef_by_octave(octave)
-            leaves = get_leaves(staff)
-            if not leaves:
-                continue
-            first_leaf = leaves[0]
-            attach(clef, first_leaf)
 
     @staticmethod
     def _get_staff_name(name: str | None) -> str:
@@ -350,72 +382,6 @@ class Notation:
             staff.append(matrix_note)
 
     @staticmethod
-    def _get_first_staff_leaf(staff: Staff) -> Leaf | None:
-        leaves = get_leaves(staff)
-        if not leaves:
-            return None
-        return leaves[0]
-
-    @classmethod
-    def _set_staff_instrument_name(cls, staff: Staff):
-        instrument_name = staff.name or ""
-        instrument_name = instrument_name.title()
-        first_leaf = cls._get_first_staff_leaf(staff)
-        attach(InstrumentName(instrument_name), first_leaf)
-        attach(ShortInstrumentName(instrument_name), first_leaf)
-
-    @classmethod
-    def _get_part_staves(cls, passage: Passage) -> tuple[Staff, Staff]:
-        bass = passage.bass_staff
-        melody = passage.melody_staff
-        for staff in bass, melody:
-            cls._set_staff_instrument_name(staff)
-        return bass, melody
-
-    @classmethod
-    def _get_rest(
-        cls, duration: Duration, is_multi_measure_rest: bool
-    ) -> Rest | MultimeasureRest:
-        if is_multi_measure_rest:
-            return cast(
-                MultimeasureRest,
-                multiplied_duration([duration], MultimeasureRest)[0],
-            )
-        return Rest(duration)
-
-    @staticmethod
-    def _add_leaf_to_staff(
-        staff_group: StaffGroup,
-        instrument_name: str,
-        leaf: Leaf,
-        tuplet: Tuplet | None,
-        is_start_of_tuplet: bool,
-    ):
-        if tuplet and is_start_of_tuplet:
-            multiplier = tuplet.multiplier
-            component: Leaf | Tuplet = Tuplet(multiplier, components=[leaf])
-        else:
-            component = leaf
-        staff = next(
-            (staff for staff in staff_group if staff.name == instrument_name),
-            None,
-        )
-        if staff:
-            if tuplet and not is_start_of_tuplet:
-                parent = get_tuplets(staff)[-1]
-            else:
-                parent = staff
-            parent.append(component)
-        else:
-            staff = Staff([component], name=instrument_name)
-            instrument_name_markup = f"\\markup { {instrument_name} }"
-            instrument_name_markup = instrument_name_markup.replace("'", "")
-            first_leaf = staff[0]
-            attach(InstrumentName(instrument_name_markup), first_leaf)
-            attach(ShortInstrumentName(instrument_name_markup), first_leaf)
-            staff_group.insert(0, staff)
-
-    @staticmethod
     def _add_double_bar_lines(staff_group: StaffGroup):
         for staff in staff_group:
             leaves = get_leaves(staff)
@@ -424,49 +390,98 @@ class Notation:
             last_leaf = leaves[-1]
             attach(BarLine("|."), last_leaf)
 
+    @classmethod
+    def _set_staff_group_clefs(cls, staff_group: StaffGroup):
+        staves = [
+            staff
+            for staff in staff_group
+            if staff.name not in ["bass", "melody"]
+        ]
+        for staff in staves:
+            written_pitches = [note.written_pitch for note in get_notes(staff)]
+            pitches = [note for note in written_pitches if note]
+            octaves = [pitch.octave.number for pitch in pitches]
+            octave = mode(octaves)
+            clef = cls._get_clef_by_octave(octave)
+            leaves = get_leaves(staff)
+            if not leaves:
+                continue
+            first_leaf = leaves[0]
+            attach(clef, first_leaf)
+
+    def _get_lilypond_preamble(self, full_score=False) -> str:
+        passage = self._passage
+        if passage:
+            title = passage.title
+            composer = passage.composer
+        else:
+            if self._number_of_matrices > 1:
+                matrix_display = "Matrices"
+            else:
+                matrix_display = "Matrix"
+            title = f"Combination-Tone {matrix_display}"
+            composer = ""
+        if full_score:
+            stencils = ""
+        else:
+            stencils = """
+                \\override TimeSignature.stencil = ##f
+                \\override BarLine.stencil = ##f
+                \\override Stem.stencil = ##f
+            """
+        return f"""
+                    \\header {{
+                        tagline = ##f
+                        title = "{title}"
+                        composer = "{composer}"
+                    }}
+
+                    \\paper {{
+                        #(set-paper-size "letter")
+                        left-margin = 1.25\\in
+                        right-margin = 0.75\\in
+                        top-margin = 0.5\\in
+                        bottom-margin = 0.5\\in
+                    }}
+
+                    \\layout {{
+                        \\context {{
+                            \\Score
+                            \\numericTimeSignature
+                            {stencils}
+                        }}
+                    }}
+                """
+
+    def _show_with_preamble(self, score: Score, save: bool, full_score: bool):
+        lilypond_file = LilyPondFile(
+            [self._get_lilypond_preamble(full_score), score]
+        )
+        if save:
+            pdf_file_path = Path("examples") / "matrix.pdf"
+            as_pdf(lilypond_file, pdf_file_path=pdf_file_path, remove_ly=True)
+        else:
+            show(lilypond_file)
+
     def _make_ensemble_score(
-        self,
-        tuning: Tuning,
-        save: bool,
-        full_score: bool,
+        self, tuning: Tuning, save: bool, full_score: bool
     ):
         staff_group = StaffGroup()
         passage = self._passage
         if passage:
-            bass, melody = self._get_part_staves(passage)
-            for staff in melody, bass:
+            for staff in self._input_staves:
                 staff_group.append(staff)
-            for matrix_leaf in track(
+            matrix_leaves = track(
                 passage.matrix_leaves, description=self.PROGRESS_DESCRIPTION
-            ):
+            )
+            for matrix_leaf in matrix_leaves:
                 duration = matrix_leaf.duration
                 if not duration:
                     continue
                 if matrix_leaf.contains_pitches:
-                    for matrix_pitch in matrix_leaf.generated_pitches:
-                        note = matrix_pitch.get_note(duration, matrix_leaf.tie)
-                        if not note:
-                            continue
-                        instrument_names = matrix_pitch.get_instrument_name()
-                        self._add_leaf_to_staff(
-                            staff_group,
-                            instrument_names,
-                            note,
-                            matrix_leaf.tuplet,
-                            matrix_leaf.is_start_of_tuplet,
-                        )
+                    self._add_notes_to_staff(matrix_leaf, staff_group)
                 else:
-                    for instrument_names in matrix_leaf.instrument_names:
-                        rest = self._get_rest(
-                            duration, matrix_leaf.is_multi_measure_rest
-                        )
-                        self._add_leaf_to_staff(
-                            staff_group,
-                            instrument_names,
-                            rest,
-                            matrix_leaf.tuplet,
-                            matrix_leaf.is_start_of_tuplet,
-                        )
+                    self._add_rests_to_staff(matrix_leaf, staff_group)
         else:
             for matrix in track(
                 self._matrices, description=self.PROGRESS_DESCRIPTION
