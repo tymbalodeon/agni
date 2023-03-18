5c82557134572af3b079866616b68b667bd801f1
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-03-17 21:35:29 -0400

    add tests to 50% coverage

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -53,3 +53,135 @@ def test_matrix_command():
     actual_lines = actual_output.split("\n")
     for display_line, expected_line in zip(expected_lines, actual_lines):
         assert display_line.strip() == expected_line.strip()
+
+
+def test_matrix_multiples_3():
+    bass = "440"
+    melody = "466"
+    multiples = "3"
+    expected_text = """Combination-Tone Matrix (Hertz)
+
+             0 x melody   1 x melody   2 x melody
+  0 x bass                466.0        932.0
+  1 x bass   440.0        906.0        1,372.0
+  2 x bass   880.0        1,346.0      1,812.0
+    """
+    actual_output = call_command(
+        [matrix_command, bass, melody, "--multiples", multiples]
+    )
+    expected_lines = expected_text.split("\n")
+    actual_lines = actual_output.split("\n")
+    for display_line, expected_line in zip(expected_lines, actual_lines):
+        assert display_line.strip() == expected_line.strip()
+
+
+def test_matrix_pitch_type_lilypond():
+    bass = "440"
+    melody = "466"
+    pitch_type = "lilypond"
+    expected_text = """Combination-Tone Matrix (Lilypond)
+
+             0 x melody   1 x melody   2 x melody   3 x melody
+  0 x bass                bf'          bf''         f'''
+  1 x bass   a'           aqs''        f'''         bf'''
+  2 x bass   a''          e'''         aqs'''       dqf''''
+  3 x bass   e'''         a'''         cs''''       eqs''''
+    """
+    actual_output = call_command(
+        [matrix_command, bass, melody, "--pitch-type", pitch_type]
+    )
+    expected_lines = expected_text.split("\n")
+    actual_lines = actual_output.split("\n")
+    for display_line, expected_line in zip(expected_lines, actual_lines):
+        assert display_line.strip() == expected_line.strip()
+
+
+def test_matrix_pitch_type_midi():
+    bass = "440"
+    melody = "466"
+    pitch_type = "midi"
+    expected_text = """Combination-Tone Matrix (Midi)
+
+             0 x melody   1 x melody   2 x melody   3 x melody
+  0 x bass                70.0         82.0         89.0
+  1 x bass   69.0         81.5         88.5         94.0
+  2 x bass   81.0         88.5         93.5         97.5
+  3 x bass   88.0         93.5         97.5         100.5
+    """
+    actual_output = call_command(
+        [matrix_command, bass, melody, "--pitch-type", pitch_type]
+    )
+    expected_lines = expected_text.split("\n")
+    actual_lines = actual_output.split("\n")
+    for display_line, expected_line in zip(expected_lines, actual_lines):
+        assert display_line.strip() == expected_line.strip()
+
+
+def test_matrix_pitch_type_all():
+    bass = "440"
+    melody = "466"
+    pitch_type = "all"
+    expected_text = """Combination-Tone Matrix (All)
+
+             0 x melody   1 x melody   2 x melody   3 x melody
+  0 x bass                466.0        932.0        1,398.0
+                          bf'          bf''         f'''
+                          70.0         82.0         89.0
+  1 x bass   440.0        906.0        1,372.0      1,838.0
+             a'           aqs''        f'''         bf'''
+             69.0         81.5         88.5         94.0
+  2 x bass   880.0        1,346.0      1,812.0      2,278.0
+             a''          e'''         aqs'''       dqf''''
+             81.0         88.5         93.5         97.5
+  3 x bass   1,320.0      1,786.0      2,252.0      2,718.0
+             e'''         a'''         cs''''       eqs''''
+             88.0         93.5         97.5         100.5
+    """
+    actual_output = call_command(
+        [matrix_command, bass, melody, "--pitch-type", pitch_type]
+    )
+    expected_lines = expected_text.split("\n")
+    actual_lines = actual_output.split("\n")
+    for display_line, expected_line in zip(expected_lines, actual_lines):
+        assert display_line.strip() == expected_line.strip()
+
+
+def test_matrix_tuning_equal_tempered():
+    bass = "440"
+    melody = "466"
+    tuning = "equal-tempered"
+    expected_text = """Combination-Tone Matrix (Hertz)
+
+             0 x melody   1 x melody   2 x melody   3 x melody
+  0 x bass                466          932          1,398
+  1 x bass   440          906          1,372        1,838
+  2 x bass   880          1,346        1,812        2,278
+  3 x bass   1,320        1,786        2,252        2,718
+    """
+    actual_output = call_command(
+        [matrix_command, bass, melody, "--tuning", tuning]
+    )
+    expected_lines = expected_text.split("\n")
+    actual_lines = actual_output.split("\n")
+    for display_line, expected_line in zip(expected_lines, actual_lines):
+        assert display_line.strip() == expected_line.strip()
+
+
+def test_matrix_midi_input():
+    bass = "440"
+    melody = "466"
+    expected_text = """Combination-Tone Matrix (Midi)
+
+             0 x melody   1 x melody   2 x melody   3 x melody
+  0 x bass                466.0        478.0        485.0
+  1 x bass   440.0        469.5        480.0        486.5
+  2 x bass   452.0        472.5        481.5        487.5
+  3 x bass   459.0        475.0        483.0        488.5
+    """
+    actual_output = call_command(
+        [matrix_command, bass, melody, "--midi-input"]
+    )
+    expected_lines = expected_text.split("\n")
+    actual_lines = actual_output.split("\n")
+    for display_line, expected_line in zip(expected_lines, actual_lines):
+        assert display_line.strip() == expected_line.strip()
